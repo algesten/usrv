96a736950b0ea552caca94aaf31de0ba30b83692
Martin Algesten <martin@algesten.se>
2025-02-07 21:57:27 +0100

    Model

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,26 @@
-pub fn holding_crate_for_usrv() {}
+#![forbid(unsafe_code)]
+#![warn(clippy::all)]
+// #![deny(missing_docs)]
+
+#[macro_use]
+extern crate log;
+
+pub use ureq_proto::http;
+
+mod arg;
+mod handler;
+mod into_res;
+mod matcher;
+mod router;
+mod send_body;
+mod service;
+mod util;
+
+pub use arg::{Arg, RefArg};
+pub use handler::Handler;
+pub use into_res::{IntoResponse, NotFound};
+pub use router::{MethodHandler, MethodRouter, Router};
+pub use send_body::{IntoSendBody, SendBody};
+pub use service::Service;
+
+pub struct Body;
---
+++ src/service.rs
@@ -0,0 +1,35 @@
+use std::marker::PhantomData;
+
+use crate::http::{Request, Response};
+use crate::router::{CallResult, Callable};
+use crate::{Body, IntoResponse, NotFound, SendBody};
+
+pub struct Service<S, R> {
+    _state: PhantomData<S>,
+    router: R,
+}
+
+impl<S, R: Callable<S>> Service<S, R> {
+    pub(crate) fn new(router: R) -> Self {
+        Service {
+            _state: PhantomData,
+            router,
+        }
+    }
+
+    pub fn call(&self, state: S, request: Request<Body>) -> Response<SendBody> {
+        match self.router.call(state, request) {
+            CallResult::Handled(v) => v,
+            CallResult::Unhandled(_, _) => NotFound.into_response(),
+        }
+    }
+}
+
+impl<S, P: Clone> Clone for Service<S, P> {
+    fn clone(&self) -> Self {
+        Self {
+            _state: PhantomData,
+            router: self.router.clone(),
+        }
+    }
+}
